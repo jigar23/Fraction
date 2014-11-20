fe6454da634306aa03604737ee41668e9dd22fe2
Jigar <jigargada1990@gmail.com>
2014-11-19 17:50:54 -0800

    Add your code here

---
+++ main.cc
@@ -0,0 +1,16 @@
+/*
+ * main.cc
+ *
+ *  Created on: Nov 19, 2014
+ *      Author: jigar
+ */
+
+#include <iostream>
+#include "src/Fraction.h"
+
+int main()
+{
+	//Write all your code here
+
+	return 0;
+}
